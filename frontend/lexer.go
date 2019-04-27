bc97d263918e53e54b2177bcd607774898bacfc1
Ian Leinbach <ialeinbach@vassar.edu>
2019-04-27 02:26:22 -0400

    cleanup, renaming, and commenting

--- frontend/lexer.go
+++ frontend/lexer.go
@@ -1,28 +1,34 @@
 package frontend
 
-//go:generate goyacc -l -o parser.go parser.y
-
 import (
 	"imp/errors"
-	"imp/internal"
 )
 
 const (
-	MaxCmdLength int = 16
-	MaxRegLength int = 8
-	MaxNumLength int = 21 // can hold all int64 values (incl. sign)
+	maxCmdLength int = 32
+	maxRegLength int = 32
+	maxNumLength int = 32
 
-	NumPrefix rune = '#'
-	RegPrefix rune = '@'
+	numPrefix rune = '#'
+	regPrefix rune = '@'
 )
 
+//
+// Lexer
+//
+
+// Implements goyacc's yyLexer interface.
 type lexer struct {
+	// Source code being lexed.
+	input string
+
+	// Indices into input used to build a lexeme for tokenization.
 	start int
 	curr  int
-	input string
-	line  int
-	err   error
-	ret   []internal.Stmt
+
+	// Lexer state for debugging.
+	line int
+	err  error
 }
 
 type Token struct {
@@ -31,12 +37,28 @@ type Token struct {
 }
 
 func Lexer(input string) *lexer {
-	return &lexer{ input: input, line: 1 }
+	return &lexer{
+		input: input,
+		line:  1,
+	}
 }
 
-// No lexerPred closures allowed.
+func (l *lexer) head() rune {
+	return rune(l.input[l.curr])
+}
+
+func (l *lexer) lexeme() string {
+	return string(l.input[l.start:l.curr])
+}
+
+//
+// Lexer Predicate Functions
+//
+
 type lexerPred func(rn rune) bool
 
+// Primitives that are composed to build functions that define the textual
+// representation of imp data.
 var (
 	predDec lexerPred = func(rn rune) bool {
 		return rn >= '0' && rn <= '9'
@@ -52,49 +74,66 @@ var (
 			(rn >= 'a' && rn <= 'z') ||
 			(rn >= 'A' && rn <= 'Z') )
 	}
+)
+
+var (
+	// Cmds are alphanumeric with underscores.
 	predCmdLiteral lexerPred = func(rn rune) bool {
-		return predAlpha(rn) || predDec(rn) || rn == '_' || rn == '?'
+		return predAlpha(rn) || predDec(rn) || rn == '_'
 	}
-	predRegLiteral lexerPred = func(rn rune) bool {
-		return predAlpha(rn) || predHex(rn)
-	}
-	predNumLiteral lexerPred = func(rn rune) bool {
-		return predAlpha(rn) || predHex(rn)
-	}
+
+	// Reg aliases are alphanumeric and Reg literals are numeric.
 	predRegPrefix lexerPred = func(rn rune) bool {
-		return rn == RegPrefix
+		return rn == regPrefix
 	}
+	predRegBody lexerPred = func(rn rune) bool {
+		return predAlpha(rn) || predHex(rn)
+	}
+
+	// Num aliases are alphanumeric and Num literals are numeric.
 	predNumPrefix lexerPred = func(rn rune) bool {
-		return rn == NumPrefix
+		return rn == numPrefix
+	}
+	predNumBody lexerPred = func(rn rune) bool {
+		return predAlpha(rn) || predHex(rn)
 	}
 )
 
-func (l *lexer) emit(tokName string, lval *yySymType) {
-	lexeme := l.lexeme()
+//
+// Lexer Driver Methods
+//
 
-	errors.DebugLexer(1, true, "Emitting %s(%s)\n", tokName, lexeme)
+// Emits a token to the parser. Called for non-trivial tokens i.e. ones that
+// have a corresponding item on the goyacc value stack.
+func (l *lexer) emit(name string, lval *yySymType) {
+	errors.DebugLexer(1, true, "Emitting %s(%s)\n", name, l.lexeme())
+	errors.DebugLexer(2, true, "====================\n")
+	errors.DebugLexer(2, false, "\n")
 
-//	if LexerVerbosityFlag == 1 {
-//		fmt.Printf("\n")
-//	}
+	// Tokenize lexeme and emit.
 	lval.tok = Token{
-		Lexeme: lexeme,
+		Lexeme: l.lexeme(),
 		Line:   l.line,
 	}
+
+	// Move past consumed lexeme.
 	l.start = l.curr
 
-	errors.DebugLexer(2, true, "====================\n\n")
+	return
 }
 
+// Advances lexer according to pred for at most max characters. Returns the number of
+// characters by which the lexer was advanced.
 func (l *lexer) lexPred(pred lexerPred, max int) (n int) {
 	errors.DebugLexer(2, true, "--------------------\n")
-	errors.DebugLexer(2, true, "Reading '%s'\t", errors.Repr(rune(l.input[l.curr])))
+	errors.DebugLexer(2, true, "Reading '%s'\t", errors.Repr(l.head()))
 
-	for len(l.input[l.curr:]) > 0 && n < max && pred(rune(l.input[l.curr])) {
-		errors.DebugLexer(2, false, "  OK\n")
+	for len(l.input[l.curr:]) > 0 && n < max && pred(l.head()) {
 		l.curr++
-		errors.DebugLexer(2, true, "Reading '%s'\t", errors.Repr(rune(l.input[l.curr])))
 		n++
+
+		errors.DebugLexer(2, false, "  OK\n")
+		errors.DebugLexer(2, true, "Reading '%s'\t", errors.Repr(l.head()))
 	}
 
 	errors.DebugLexer(2, false, "FAIL\n")
@@ -103,28 +142,33 @@ func (l *lexer) lexPred(pred lexerPred, max int) (n int) {
 	return
 }
 
-func (l *lexer) lexPrefixed(prefix lexerPred, suffix lexerPred, max int) (n int) {
+// Advances lexer with prefix predicate for first character, then uses body
+// predicate for at most max characters. The prefix symbol does not count
+// towards the character count as it pertains to max.
+func (l *lexer) lexPrefixed(prefix lexerPred, body lexerPred, max int) (n int) {
 	defer func() {
-		// ignore prefix
 		switch {
+		// Ignore prefix if successful.
 		case n > 1:
 			l.start++
 			n -= 1
+		// Rewind lexer if we only saw prefix.
 		case n == 1:
 			l.curr--
 			n = 0
 		}
 	}()
 
-	// declaration must be referencable from definition
+	// Must be self-referentiable.
 	var pred lexerPred
 
-	// lex prefix then pass baton to suffix
+	// Lex prefix then replace self in order to lex body.
 	pred = func(rn rune) (ok bool) {
-		defer func() { pred = suffix }()
+		defer func() { pred = body }()
 		return prefix(rn)
 	}
 
+	// Wrap in a closure so caller is none the wiser.
 	return l.lexPred(func(rn rune) bool {
 		return pred(rn)
 	}, max)
@@ -132,48 +176,61 @@ func (l *lexer) lexPrefixed(prefix lexerPred, suffix lexerPred, max int) (n int)
 
 func (l *lexer) lexCmd() int {
 	errors.DebugLexer(2, true, "Lexing CMD\n")
-	return l.lexPred(predCmdLiteral, MaxCmdLength)
+	return l.lexPred(predCmdLiteral, maxCmdLength)
 }
 
 func (l *lexer) lexReg() int {
 	errors.DebugLexer(2, true, "Lexing REG\n")
-	return l.lexPrefixed(predRegPrefix, predRegLiteral, MaxRegLength)
+	return l.lexPrefixed(predRegPrefix, predRegBody, maxRegLength)
 }
 
 func (l *lexer) lexNum() int {
 	errors.DebugLexer(2, true, "Lexing NUM\n")
-	return l.lexPrefixed(predNumPrefix, predNumLiteral, MaxNumLength)
+	return l.lexPrefixed(predNumPrefix, predNumBody, maxNumLength)
 }
 
-func (l *lexer) lexeme() string {
-	return string(l.input[l.start:l.curr])
-}
+//
+// Handles For Goyacc
+//
 
 // Satisfies yyLexer.
 func (l *lexer) Lex(lval *yySymType) int {
 	var ch int
+
 	for len(l.input[l.curr:]) > 0 {
-		ch = int(l.input[l.curr])
-		switch ch {
+		switch ch = int(l.head()); ch {
+
+		// Ignore whitespace within a given line.
 		case ' ', '\t':
 			l.curr++
 			l.start++
 			continue
+
+		// Newlines are slightly more important whitespace (for collecting line
+		// info and imp syntax).
 		case '\n':
-			errors.DebugLexer(1, true, "Emitting CR\n\n")
 			l.curr++
 			l.start++
 			l.line++
+
+			errors.DebugLexer(1, true, "Emitting CR\n")
 			return CR
+
+		// Lex trivial tokens. Used as-is in parser i.e. no corresponding item
+		// on goyacc value stack.
 		case ':', ',', '{', '}':
-			errors.DebugLexer(1, true, "Emitting SYM(%c)\n\n", ch)
 			l.curr++
 			l.start++
+
+			errors.DebugLexer(1, true, "Emitting SYM(%c)\n", ch)
 			return ch
 		}
 
+		errors.DebugLexer(2, false, "\n")
 		errors.DebugLexer(2, true, "====================\n")
 
+		// Lex non-trivial tokens (i.e. can be directly mapped to nodes on the
+		// AST). Pass control of lexer to its methods.
 		switch {
 		case l.lexCmd() > 0:
 			l.emit("CMD", lval)
@@ -189,6 +246,7 @@ func (l *lexer) Lex(lval *yySymType) int {
 		l.Error(errors.UnrecognizedInput(rune(ch)))
 		return ch
 	}
+
 	return ch
 }
 
--- frontend/parser.y
+++ frontend/parser.y
@@ -4,24 +4,28 @@ package frontend
 
 import (
 	"imp/errors"
-	"imp/internal"
+	"imp/backend"
 )
 
-func Parse(input string) ([]internal.Stmt, error) {
+// yyParse can only return an int. Final grammar rule places AST here so that
+// Parse() can return it.
+var abstractSyntaxTree []backend.Stmt
+
+func Parse(input string) ([]backend.Stmt, error) {
 	l := Lexer(input)
 	yyParse(l)
 	if l.err != nil {
 		return nil, l.err
 	}
-	return l.ret, nil
+	return abstractSyntaxTree, nil
 }
 
 %}
 
 %union{
 	tok      Token
-	arglist  []internal.Alias
-	stmtlist []internal.Stmt
+	arglist  []backend.Alias
+	stmtlist []backend.Stmt
 }
 
 %token <tok> CMD REG NUM CR
@@ -35,41 +39,43 @@ func Parse(input string) ([]internal.Stmt, error) {
 
 main:
 	program {
-		errors.DebugParser(1, true, "main -> program\n\n")
-		errors.DebugParser(2, false, internal.DumpAst($1) + "\n\n")
+		// yyParse can only return an int. Place AST in package global so that
+		// Parse() can return it.
+		abstractSyntaxTree = $1
 
-		// Work around yyParse return value.
-		yylex.(*lexer).ret = $1
+		errors.DebugParser(1, true, "main -> program\n")
+		errors.DebugParser(2, false, "\n")
+		errors.DebugParser(2, true, backend.DumpAst($1))
+		errors.DebugParser(2, false, "\n\n")
 	}
 
 program:
 	program stmt {
-		errors.DebugParser(1, true, "program -> program delim stmt\n\n")
 		$$ = append($1, $2...)
+		errors.DebugParser(1, true, "program -> program delim stmt\n")
 	}
 |
 	stmt {
-		errors.DebugParser(1, true, "program -> stmt\n\n")
+		errors.DebugParser(1, true, "program -> stmt\n")
 		$$ = $1
 	}
 
 stmt:
 	decl delim {
-		errors.DebugParser(1, true, "stmt -> decl \n\n")
 		$$ = $1
+		errors.DebugParser(1, true, "stmt -> decl \n")
 	}
 |
 	call delim {
-		errors.DebugParser(1, true, "stmt -> call \n\n")
 		$$ = $1
+		errors.DebugParser(1, true, "stmt -> call \n")
 	}
 
 decl:
 	':' CMD args '{' delim program '}' {
-		errors.DebugParser(1, true, "decl -> :CMD args delim { program }\n\n")
-		$$ = []internal.Stmt{internal.Stmt(
-			internal.Decl{
-				Cmd: internal.CmdAlias{
+		$$ = []backend.Stmt{backend.Stmt(
+			backend.Decl{
+				Cmd: backend.CmdAlias{
 					Name: $2.Lexeme,
 					Line: $2.Line,
 				},
@@ -77,64 +83,65 @@ decl:
 				Body: $6,
 			},
 		)}
+		errors.DebugParser(1, true, "decl -> :CMD args delim { program }\n")
 	}
 
 call:
 	CMD args {
-		errors.DebugParser(1, true, "call -> CMD args\n\n")
-		$$ = []internal.Stmt{internal.Stmt(
-			internal.Call{
-				Cmd: internal.CmdAlias{
+		$$ = []backend.Stmt{backend.Stmt(
+			backend.Call{
+				Cmd: backend.CmdAlias{
 					Name: $1.Lexeme,
 					Line: $1.Line,
 				},
 				Args: $2,
 			},
 		)}
+		errors.DebugParser(1, true, "call -> CMD args\n")
 	}
 
 args:
 	/* nullable */ {
-		errors.DebugParser(1, true, "args -> EPSILON\n\n")
-		$$ = make([]internal.Alias, 0, 0)
+		$$ = make([]backend.Alias, 0, 0)
+		errors.DebugParser(1, true, "args -> EPSILON\n")
 	}
 |
 	arg ',' args {
-		errors.DebugParser(1, true, "args -> arg, args\n\n")
 		$$ = append($1, $3...)
+		errors.DebugParser(1, true, "args -> arg, args\n")
 	}
 |
 	arg {
-		errors.DebugParser(1, true, "args -> arg\n\n")
 		$$ = $1
+		errors.DebugParser(1, true, "args -> arg\n")
 	}
 
 arg:
 	REG {
-		errors.DebugParser(1, true, "arg -> REG\n\n")
-		$$ = []internal.Alias{internal.Alias(
-			internal.RegAlias{
+		$$ = []backend.Alias{backend.Alias(
+			backend.RegAlias{
 				Name: $1.Lexeme,
 				Line: $1.Line,
 			},
 		)}
+		errors.DebugParser(1, true, "arg -> REG\n")
 	}
 |
 	NUM {
-		errors.DebugParser(1, true, "arg -> NUM\n\n")
-		$$ = []internal.Alias{internal.Alias(
-			internal.NumAlias{
+		$$ = []backend.Alias{backend.Alias(
+			backend.NumAlias{
 				Name: $1.Lexeme,
 				Line: $1.Line,
 			},
 		)}
+		errors.DebugParser(1, true, "arg -> NUM\n")
 	}
 
 delim:
 	CR delim {
-		errors.DebugParser(1, true, "delim -> CR delim\n\n")
+		errors.DebugParser(1, true, "delim -> CR delim\n")
 	}
 |
 	CR {
-		errors.DebugParser(1, true, "delim -> CR\n\n")
+		errors.DebugParser(1, true, "delim -> CR\n")
 	}
